e9eb8e0c64140b4406a4cfdf5ef27d0c04d3fca1
AmeyaSingh23 <ameyapratap.cs24@bmsce.ac.in>
2026-04-14 23:17:47 +0530

    added about me section

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 window.addEventListener("scroll", function() {
     let navbar = document.querySelector(".navbar");
 
-    if (window.scrollY > 50) {
+    if (window.scrollY > 670) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
     }
 });
+
+const pills = document.querySelectorAll(".pill");
+const contents = document.querySelectorAll(".pill-content");
+
+pills.forEach(pill => {
+    pill.addEventListener("click", () => {
+
+        pills.forEach(p => p.classList.remove("active"));
+        pill.classList.add("active");
+
+        contents.forEach(content => content.classList.remove("active"));
+        const target = pill.getAttribute("data-target");
+        document.getElementById(target).classList.add("active");
+    })
+})
